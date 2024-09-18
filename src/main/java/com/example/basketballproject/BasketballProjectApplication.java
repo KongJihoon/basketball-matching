7a94f05c1b34e72e892ab938af2d37f1fcfd86ac
KongJihoon <cc127923@gmail.com>
2024-09-18 19:44:46 +0900

    first commit

---
+++ src/main/java/com/example/basketballproject/BasketballProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.basketballproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketballProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasketballProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/basketballproject/BasketballProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basketballproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketballProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
