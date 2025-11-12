ba59b123851945679d87913dd752927f470eab80
KongJihoon <cc127923@gmail.com>
2025-11-12 19:59:12 +0900

    [feat] : 지난 경기 조회 및 현재 참가 경기 조회 구현

--- src/main/java/com/example/basketballmatching/gameUsers/service/impl/GameUserServiceImpl.java
+++ src/main/java/com/example/basketballmatching/gameUsers/service/impl/GameUserServiceImpl.java
@@ -3,6 +3,8 @@ package com.example.basketballmatching.gameUsers.service.impl;
 import com.example.basketballmatching.gameCreator.entity.GameEntity;
 import com.example.basketballmatching.gameCreator.repository.GameRepository;
 import com.example.basketballmatching.gameCreator.type.MatchGenderType;
+import com.example.basketballmatching.gameUsers.dto.CurrentGameListDto;
+import com.example.basketballmatching.gameUsers.dto.LastGameListDto;
 import com.example.basketballmatching.global.dto.ApiResponse;
 import com.example.basketballmatching.global.dto.CheckResponse;
 import com.example.basketballmatching.global.exception.CustomException;
@@ -10,17 +12,18 @@ import com.example.basketballmatching.gameUsers.dto.ApplyGameUserDto;
 import com.example.basketballmatching.gameCreator.entity.ParticipantGameEntity;
 import com.example.basketballmatching.gameCreator.repository.ParticipantGameRepository;
 import com.example.basketballmatching.gameUsers.service.GameUserService;
-import com.example.basketballmatching.gameCreator.type.ParticipantGameStatus;
 import com.example.basketballmatching.user.entity.UserEntity;
 import com.example.basketballmatching.user.repository.UserRepository;
 import com.example.basketballmatching.user.type.GenderType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
+import java.util.List;
 
 import static com.example.basketballmatching.gameCreator.type.ParticipantGameStatus.*;
 import static com.example.basketballmatching.global.exception.ErrorCode.*;
@@ -73,7 +76,7 @@ public class GameUserServiceImpl implements GameUserService {
         GameEntity gameEntity = gameRepository.findByGameIdAndDeletedDateTimeIsNull(gameId)
                 .orElseThrow(() -> new CustomException(GAME_NOT_FOUND));
 
-        ParticipantGameEntity participantGameEntity = participantGameRepository.findByGameEntity_GameIdAndUserEntity_UserId(gameId, userId)
+        ParticipantGameEntity participantGameEntity = participantGameRepository.findByGameEntity_GameIdAndParticipantGameId(gameId, userId)
                 .orElseThrow(() -> new CustomException(PARTICIPANT_NOT_FOUND));
 
 
@@ -106,11 +109,53 @@ public class GameUserServiceImpl implements GameUserService {
         return CheckResponse.of(true, "경기 취소가 완료되었습니다.");
     }
 
+    @Override
+    @Transactional(readOnly = true)
+    public ApiResponse<List<CurrentGameListDto>> getMyCurrentGameList(Long userId, Pageable pageable) {
+
+        UserEntity userEntity = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
+
+        LocalDateTime now = LocalDateTime.now();
+
+        Page<ParticipantGameEntity> participantGameEntities = participantGameRepository.findByUserEntity_UserIdAndParticipantGameStatusIn(userEntity.getUserId(), List.of(ACCEPT, APPLY), pageable);
+
+        List<CurrentGameListDto> gameListDtos = participantGameEntities.stream()
+                .filter(p -> p.getGameEntity().getStartDateTime().isAfter(now))
+                .map(CurrentGameListDto::fromEntity)
+                .toList();
+
+
+        return ApiResponse.of("현재 예정된 게임 조회가 완료되었습니다.", gameListDtos);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public ApiResponse<List<LastGameListDto>> getMyLastGameList(Long userId, Pageable pageable) {
+
+        UserEntity userEntity = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
+
+        LocalDateTime now = LocalDateTime.now();
+
+        Page<ParticipantGameEntity> participantGameEntities = participantGameRepository.findByUserEntity_UserIdAndParticipantGameStatus(userEntity.getUserId(), ACCEPT, pageable);
+
+        List<LastGameListDto> gameListDtos = participantGameEntities.stream()
+                .filter(p -> p.getGameEntity().getStartDateTime().isBefore(now))
+                .map(LastGameListDto::fromEntity)
+                .toList();
+
+
+        return ApiResponse.of("지난 게임 조회가 완료되었습니다.", gameListDtos);
+    }
+
+
+
     private void validateParticipantInfo(UserEntity userEntity, GameEntity gameEntity) {
 
         LocalDateTime now = LocalDateTime.now();
 
-        if (participantGameRepository.existsByUserEntity_UserIdAndGameEntity_GameId(userEntity.getUserId(), gameEntity.getGameId())) {
+        if (participantGameRepository.existsByParticipantGameIdAndGameEntity_GameId(userEntity.getUserId(), gameEntity.getGameId())) {
             throw new CustomException(ALREADY_APPLY_GAME_USER);
         }
 
